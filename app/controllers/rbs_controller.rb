3de66289f51919025d12c1ec0820b43e4fa28092
wannabeapro <dennis-russ@web.de>
2015-12-18 06:54:49 -0800

    Baumdiagramm fixed

--- app/controllers/rbs_controller.rb
+++ app/controllers/rbs_controller.rb
@@ -54,19 +54,19 @@ class RbsController < ApplicationController
       data_table.new_column('string', 'ToolTip')
       data_table.add_row(
         [
-         {:v => @project.title, :f =>@project.title   }, @project.title, ' '
+         {:v => "p"+@project.id.to_s, :f =>@project.title   }, "p"+@project.id.to_s, ' '
         ]
       )
       @project.kinds.each do |kind|
         data_table.add_row(
           [
-            {:v => kind.name, :f =>kind.name   }, @project.title, ' '
+            {:v => "k"+kind.id.to_s, :f =>kind.name   }, "p"+@project.id.to_s, ' '
           ]
         )
         kind.roles.each do |role|
           data_table.add_row(
             [
-              {:v => role.name, :f =>role.name   }, kind.name, ' '
+              {:v => "r"+role.id.to_s, :f =>role.name   }, "k"+kind.id.to_s, ' '
             ]
           )
         end

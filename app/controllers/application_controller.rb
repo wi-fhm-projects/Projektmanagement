e25d646a016e49ec34937ca7cc39fd213879611c
Eduard <es1989@gmail.com>
2016-01-09 10:17:42 +0100

    change devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :warning, :danger, :info
   before_action :find_projects
+  before_action :authenticate_user!
 
   def find_projects
     @projects = Project.all
